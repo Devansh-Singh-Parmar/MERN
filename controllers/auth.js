bfcb198e90d548223b7f89c72d0cddce906bc074
Devansh <devansh18dp@gmail.com>
2021-04-27 18:35:28 +0530

    cutom middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -77,6 +77,23 @@ exports.signout = (req, res) => {
 exports.isSignedIn = expressJwt({
     secret: process.env.SECRET,
     userProperty: "auth"
-})
+});
 
 //custom middlwares
+exports.isAuthenticated = (req, res, next) =>{
+    let checker = req.profile && req.auth & req.profile._id === req.auth._id;
+    if (!checker){
+        return res.status(403).json({
+            error: "ACCESS DENIED"
+        });
+    }
+    next();
+}
+exports.isAdmin = (req, res, next) =>{
+    if(req.profile.role === 0){
+        return res.status(403).json({
+            error: "You are not ADMIN, ACCESS DENIED"
+        });
+    }
+    next();
+};
