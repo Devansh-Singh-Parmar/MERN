6905489f4b395751c8fe77019ccf6234c9a4ca14
Devansh Singh Parmar <devansh18dp@gmail.com>
2021-05-12 18:14:36 +0530

    Started After long

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
-const authRoutes = require("./routes/auth")
+//My Routes
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user")
 
 //DB Connection
 mongoose.connect(process.env.DATABASE, {
@@ -27,6 +29,7 @@ app.use(cors());
 
 //My Routes
 app.use("/api", authRoutes);
+app.use("/api", userRoutes);
 
 //PORT
 const port = process.env.PORT || 8000;
---
+++ routes/user.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const router = express.Router()
+
+const  {} = require("../controllers/user")
+const  {} = require("../controllers/auth")
+
+module.exports  = router;
